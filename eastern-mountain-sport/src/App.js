fb62bd7da9708a946d5ddd234e957c6be13275c1
AyushiBajpai94 <ayushi.bajpai16@gmail.com>
2023-03-30 22:57:17 +0530

    day-3 task completed

--- eastern-mountain-sport/src/App.js
+++ eastern-mountain-sport/src/App.js
@@ -8,13 +8,10 @@ import { Navbar } from './Components/Navbar';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-         
-=======
+
 
       <Navbar/>
 
->>>>>>> 621c814f10821109f0badb45bf68cc272d42f7a6
     </div>
   );
 }
