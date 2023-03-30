352ac27b057d9a08fa93fa2e386f756c27e61213
Laxmi sanjay choudhari <laxmichoudhari04@gmail.com>
2023-03-30 22:52:46 +0530

    data added

---
+++ eastern-mountain-sport/src/Redux/ProductReducer/reducer.js
@@ -0,0 +1,21 @@
+import { PRODUCT_FAILURE, PRODUCT_REQUEST, PRODUCT_SUCCESS } from "./actionTypes";
+
+const initialState = {
+    isLoading: false,
+    isError: false,
+    products: [],   
+}
+
+export const reducer = (state = initialState, { type, payload }) => {
+    // console.log("payload",payload)
+    switch (type) {
+        case PRODUCT_REQUEST:
+            return { ...state, isLoading: true }
+        case PRODUCT_SUCCESS:
+            return { ...state, isLoading: false, products: payload }
+        case PRODUCT_FAILURE:
+            return {...state, isLoading: false, isError: true}
+        default: return state;
+    }
+}
+
