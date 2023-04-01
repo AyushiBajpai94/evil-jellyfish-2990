7dba8ed175ef993fb11d0bcb47ee1cc98ca6b419
Mnv17 <maanavverma0@gmail.com>
2023-04-01 23:32:17 +0530

    day-5 work

--- eastern-mountain-sport/src/App.js
+++ eastern-mountain-sport/src/App.js
@@ -2,11 +2,13 @@ import AdminRoutes from './admin/pages/AdminRoutes';
 import './App.css';
 import Homepage from './Pages/HomePage';
 import Checkout from './Pages/CartPage/Checkout';
+import { AllRoutes } from './Pages/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      {/* <Homepage/> */}
+      <AllRoutes/>
       {/* <AdminRoutes/> */}
     </div>
   );
---
+++ eastern-mountain-sport/src/Pages/AllRoutes.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route } from "react-router-dom"
+import Login from "./Login"
+import Homepage from "./HomePage"
+import Mens from "./Mens"
+import CartPage from "./CartPage/CartPage"
+import Checkout from "./CartPage/Checkout"
+
+export const AllRoutes = () =>{
+    return (
+        <>
+        <Routes>
+            <Route path="/" element={<Homepage/>}></Route>
+            <Route path='/login' element={<Login />}/>
+            <Route path="/mens" element={<Mens/>}></Route>
+            <Route path="/womens" element={<Mens/>}></Route>
+            <Route path="/cart" element={<CartPage/>}></Route>
+            <Route path="/cart/checkout" element={<Checkout/>}></Route>
+        </Routes>
+        </>
+    )
+}
