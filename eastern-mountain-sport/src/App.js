b116deecb2ecf9972fb1f12c8596f950432b97fa
VinodHadmode <vinodhadmode@gmail.com>
2023-04-01 23:05:38 +0530

    work-done

--- eastern-mountain-sport/src/App.js
+++ eastern-mountain-sport/src/App.js
@@ -1,14 +1,18 @@
 import AdminRoutes from './admin/pages/AdminRoutes';
 import './App.css';
 import Homepage from './Pages/HomePage';
-import Checkout from './Pages/CartPage/Checkout';
+import Checkout from './Pages/CheckoutPage/Checkout';
 import Login from './Pages/Login';
 import Register from './Pages/RegisterPage';
+import CartPage from './Pages/CartPage/CartPage';
+
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      {/* <Homepage/> */}
       {/* <AdminRoutes/> */}
+      
+
     </div>
   );
 }
